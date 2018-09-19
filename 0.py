501d6d96b0ed7355a1615a103fd9f64d6b3997c6
Cao <pec12003@ad.uconn.edu>
2018-09-18 23:29:46 -0400

    add data

---
+++ 0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 18 22:45:03 2018
+
+@author: pec12003
+"""
+
+#Parsing WebRobot Database
+
+import pandas as pd
+from sklearn.externals import joblib #similar to pickle
+
+df = pd.read_csv('Indiegogo.csv')
+
+
